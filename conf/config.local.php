68ae4e515de6514134fbca04cae8b380d5ad7e06
Ilya Semakin <il.semakin@mail.ru>
2024-05-10 01:57:49 +0300

    Templates for setting up a mail server

---
+++ conf/config.local.php
@@ -0,0 +1,9 @@
+<?php
+
+$CONF['configured'] = true;
+$CONF['default_language'] = 'ru';
+$CONF['database_password'] = 'PostfixAdmin!';
+$CONF['emailcheck_resolve_domain']='NO';
+$CONF['setup_password'] = '$2y$10$.VTfK3X6ZFj8D8JoGXbycO3tV5YFarSPSyiegkFeAS.ySUJYMgAO.';
+
+?>
